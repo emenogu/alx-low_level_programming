029f04db89df47b77fdc20332c2cd1b172e58c2e
emenogu <uchen4all@gmail.com>
2022-06-16 18:30:05 +0100

    My Task

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -7,29 +7,21 @@
 int main(void)
 {
 	int n;
-	
-	n = 1;
-	printf("%d", n);
-	for (n = 2; n <= 100; n++)
-	{
+	for (n = 1; n <= 100; n++)
 		if ((n % 3 == 0) && (n % 5 == 0))
 		{
-			printf(" FizzBuzz");
+			printf("FizzBuzz ", n);
 		}
 		else if (n % 3 == 0)
 		{
-			printf(" Fizz");
+			printf("Fizz ", n);
 		}
 		else if (n % 5 == 0)
 		{
-			printf(" Buzz");
+			printf("Buzz ", n);
 		}
 		else
 		{
-			printf(" %d", n)
+			printf("%d ", n);
 		}
-
-	}
-	printf("\n");
-	return (0);
 }
